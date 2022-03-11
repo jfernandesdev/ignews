a9d80a5de561b20fcc9564a92efb4c9b297bf5ca
jfernandesdev <jfernandes.dev@gmail.com>
2022-03-11 19:31:16 -0300

    sign in button component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import styles from './styles.module.scss';
 
+import { SignInButton } from '../SignInButton';
+
 export function Header() {
   return (
     <header className={styles.headerContainer}>
@@ -10,6 +12,8 @@ export function Header() {
           <a href="#" className={styles.active}>Home</a>
           <a href="#">Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
