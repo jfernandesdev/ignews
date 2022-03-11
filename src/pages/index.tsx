adde415becc70ea976247590c2e595d67c1bb625
jfernandesdev <jfernandes.dev@gmail.com>
2022-03-11 20:07:03 -0300

    home page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | ig.News</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about <br />the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
