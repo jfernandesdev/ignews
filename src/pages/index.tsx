4793b24db961f9b2d462324500581e1d125b8dbc
jfernandesdev <jfernandes.dev@gmail.com>
2022-03-11 14:36:09 -0300

    added typescript, global styling with sass set and external font imported

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.News</title>
+      </Head>
+      <h1>Hello World!</h1>
+    </>
+  )
+}
