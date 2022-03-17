5cd2a76b2df0a968c537eadbaa2f3f8cdc781b42
jfernandesdev <jfernandes.dev@gmail.com>
2022-03-17 09:07:08 -0300

    redirection setup for checkout and duplicate handling in Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,8 @@
+import { useSession, signIn } from 'next-auth/react';
+
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,10 +10,33 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  async function handleSubscribe() {
+    if(!session) {
+      signIn('github');
+      return;
+    }
+
+    //checkout session
+    try {
+     const response = await api.post('/subscribe')
+
+     const { sessionId } = response.data; 
+     const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      console.log(err)
+      alert(err.message);
+    }
+  }
+
   return (
     <button 
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
