fe20af1f75c3fb967b22c02a6100b5ad559a27a3
jfernandesdev <jfernandes.dev@gmail.com>
2022-03-18 12:11:39 -0300

    menu navigation with active link component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,24 @@
-import styles from './styles.module.scss';
+import Link from 'next/link';
 
+import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SignInButton';
+
+import styles from './styles.module.scss';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Ig.News" />
-
+        
         <nav>
-          <a href="#" className={styles.active}>Home</a>
-          <a href="#">Posts</a>
+          <ActiveLink activeClassName={styles.active} href='/'>
+            <a>Home</a>
+          </ActiveLink>
+
+          <ActiveLink activeClassName={styles.active} href='/posts' prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
